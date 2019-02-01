25f3e7b1ce067e7cdbfc123f4f686d36843b2f01
DevonO <do@devono.lan>
2019-01-31 23:16:57 -0500

    added first two figures and Rscripts

---
+++ scripts/R_scripts/figureScript_librarySizesByFilterMethod.R
@@ -0,0 +1,51 @@
+library(tidyverse)
+library(scales)
+
+# create theme function for all plots
+# theme function for custom plot style:
+theme_devon <- function () { 
+  theme_bw(base_size=12, base_family="Avenir") %+replace% 
+    theme(
+      panel.background  = element_blank(),
+      plot.background = element_rect(fill="transparent", colour=NA), 
+      legend.background = element_rect(fill="transparent", colour=NA),
+      legend.key = element_rect(fill="transparent", colour=NA)
+    )
+}
+
+## read in data and calculate sum of reads per Library, per Method
+df <- read_csv("https://github.com/devonorourke/tidybug/raw/master/data/text_tables/all.filtmethods.df.csv.gz")
+LibSums <- df %>% group_by(Method, Library) %>% summarize(TrimSum=sum(Reads))
+
+## add in raw read sums per library; raw "Joined" values assigned above derived by exporting "$LIB".joind.seqs.qza and ..
+## ..counting number of N/4 lines of all data. see "seqfilter.qfilt_example.sh" for details
+## operations were: 1) qiime tools export --import-path "$LIB".joind.seqs.qza --output-path joind
+## and  ...       : 2) cat ./joind/*.gz | zcat | awk '{s++}END{print s/4}'
+LibSums$JoindReads <- ""
+LibSums$JoindReads[LibSums$Library=="libA"] <- 14724385
+LibSums$JoindReads[LibSums$Library=="libB"] <- 17071355
+LibSums$JoindReads[LibSums$Library=="libC"] <- 14279338
+LibSums$JoindReads[LibSums$Library=="libD"] <- 14171652
+LibSums$JoindReads <- as.numeric(LibSums$JoindReads) 
+
+## calculate percent of reads retained per Library per Method
+LibSums <- LibSums %>% mutate(ReadsRetained = TrimSum/JoindReads)
+LibSums$Method <- factor(LibSums$Method,levels = c("vsearch", "dada2", "deblur"))
+
+## plot; exported at 750x300
+ggplot(data = LibSums, aes(x = ReadsRetained, y = Method, label=Library)) + 
+  geom_text_repel(vjust=1, nudge_y = 0.2, segment.alpha = 0.2) +
+  geom_point() +
+  #geom_point(aes(shape=Library), size=4, alpha=1) +
+  scale_shape_manual(values=c(65,66,67,68)) +
+  #scale_shape_manual(values=c(0,1,2,5)) +
+  geom_line(aes(group = Method), color="gray50") +
+  labs(title="", y="", x="fraction of retained sequences") +
+  theme_devon()
+
+## calculate percentage of reads retained per Method (aggregate all Libraries)
+## not used for plots; just used for calculation...
+MethodSums <- df %>% group_by(Method) %>% summarize(TrimSum=sum(Reads))
+JoinedReadSums <- sum(14724385, 17071355, 14279338, 14171652)
+MethodSums$JoinedReadSums <- JoinedReadSums
+MethodSums <- MethodSums %>% mutate(ReadsRetained = TrimSum/JoinedReadSums) %>% select(Method, ReadsRetained)
---
+++ scripts/R_scripts/figureScript_mockSeqCounts_byFilterMethod.R
@@ -0,0 +1,77 @@
+# Plots used in paper
+
+library(tidyverse)
+library(scales)
+library(viridis)
+library(ggridges)
+library(ggrepel)
+
+# create theme function for all plots
+# theme function for custom plot style:
+theme_devon <- function () { 
+  theme_bw(base_size=12, base_family="Avenir") %+replace% 
+    theme(
+      panel.background  = element_blank(),
+      plot.background = element_rect(fill="transparent", colour=NA), 
+      legend.background = element_rect(fill="transparent", colour=NA),
+      legend.key = element_rect(fill="transparent", colour=NA)
+    )
+}
+
+head(df)
+## import data and select mock samples:
+df <- read_csv("https://github.com/devonorourke/tidybug/raw/master/data/text_tables/all.filtmethods.df.csv.gz")
+mock <- df %>% filter(SampleType == "mock")
+## generate filtered mock data
+Sample_filtered <- df %>% group_by(SeqID) %>% summarise(SumReads=sum(Reads)) %>% filter(SumReads >= 5000) ## require at least 5000 reads per sample
+df.filt <- df %>% filter(SeqID %in% Sample_filtered$SeqID)
+rm(df)
+Hash_filtered <- df.filt %>% group_by(HashID) %>% summarise(HashCounts=n()) %>% filter(HashCounts > 1)
+df.filt <- df.filt %>% filter(HashID %in% Hash_filtered$HashID)
+mock.filt <- df.filt %>% filter(SampleType == "mock")
+
+## import mock matches (exact, partial, and misses)
+mockExact <- read_csv("https://github.com/devonorourke/tidybug/raw/master/data/mock_community/mock.exactHits.txt", col_names = FALSE)
+mockExact$Type <- "exact"
+colnames(mockExact)[1] <- "HashID"
+mockPartial <- read_csv("https://github.com/devonorourke/tidybug/raw/master/data/mock_community/mock.partialHits.txt", col_names = FALSE)
+mockPartial$Type <- "partial"
+colnames(mockPartial)[1] <- "HashID"
+mockMiss <- read_csv("https://github.com/devonorourke/tidybug/raw/master/data/mock_community/mock.partialMisses.txt", col_names = FALSE)
+mockMiss$Type <- "miss"
+colnames(mockMiss)[1] <- "HashID"
+mockHitTypes <- rbind(mockExact, mockPartial, mockMiss)
+rm(mockExact, mockPartial, mockMiss)
+
+## merge mockHitTypes $Type to mock object (by $HashID)
+mock <- merge(mock, mockHitTypes)
+mock.filt <- merge(mock.filt, mockHitTypes)
+rm(mockHitTypes)
+## add Labeler for faceting plots
+mock$Labeler <- "unfiltered"
+mock.filt$Labeler <- "filtered"
+## merge together
+mock.all <- rbind(mock, mock.filt)
+rm(mock, mock.filt)
+
+## generate 3 color palette:
+pal3 <- viridis::inferno(3, begin = 0.15, end = 0.85)
+
+## reset the levels for plot
+mock.all$Type <- factor(mock.all$Type,levels = c("exact", "partial", "miss"))
+mock.all$Library <- factor(mock.all$Library,levels = c("libA", "libB", "libC", "libD"))
+mock.all$Labeler <- factor(mock.all$Labeler,levels = c("unfiltered", "filtered"))
+
+## save as fig2A; export at EXACTLY 1000x504... any resizing screws with label positions
+ggplot(data = mock.all, aes(x = Reads, y = Method, color=Type, label = Alias)) +
+  geom_jitter(alpha=0.7, height = 0.1) +
+  scale_color_manual(values = pal3, name = "") +
+  scale_x_continuous(labels = comma, trans = "log2") +
+  facet_grid(Labeler ~ Library) +
+  #geom_text_repel(data = mock %>% filter(Type == "partial" & Reads > 2000), direction="x", size=3, vjust = 0, force = 3, nudge_y = 0.2, angle=45,segment.size = 0.2) +
+  labs(title="", x="number of sequences", y="") +
+  theme_devon() +
+  theme(legend.position="top")
+head(mock.all)
+## tiny summary of mock data, per Library, per filtering/not
+mock_Sumry <- mock.all %>% group_by(Library, Method, Labeler, Type) %>% summarise(CountTypes=n())
